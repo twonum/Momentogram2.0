4c1569ebf6eac2e75f62fb3deaf06c380b8de98c
Taha <twonuma2022@gmail.com>
2024-12-11 05:17:49 +0500

    refactor Header component to include music redirect functionality and enhance user links; add tooltip for music button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,11 +12,10 @@ import { AiFillHome } from "react-icons/ai";
 import { RxAvatar } from "react-icons/rx";
 import { Link as RouterLink } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
-import { BsFillChatQuoteFill } from "react-icons/bs";
-import { MdOutlineSettings } from "react-icons/md";
-import MusicButton from "./MusicButton"; // Import the MusicButton component
 import useLogout from "../hooks/useLogout";
 import authScreenAtom from "../atoms/authAtom";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import { MdOutlineSettings } from "react-icons/md";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -24,8 +23,13 @@ const Header = () => {
   const logout = useLogout();
   const setAuthScreen = useSetRecoilState(authScreenAtom);
 
+  const handleMusicRedirect = () => {
+    window.open("https://skycarly.onrender.com", "_blank"); // Opens the link in a new tab
+  };
+
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} mt={6} mb="12">
+      {/* Home Link */}
       {user && (
         <Link as={RouterLink} to="/">
           <AiFillHome size={24} />
@@ -41,6 +45,34 @@ const Header = () => {
         </Link>
       )}
 
+      {/* User Links */}
+      {user && (
+        <Flex alignItems={"center"} gap={4}>
+          <Link as={RouterLink} to={`/${user.username}`}>
+            <RxAvatar size={24} />
+          </Link>
+          <Link as={RouterLink} to={`/chat`}>
+            <BsFillChatQuoteFill size={20} />
+          </Link>
+          <Link as={RouterLink} to={`/settings`}>
+            <MdOutlineSettings size={20} />
+          </Link>
+          <Button size={"xs"} onClick={logout}>
+            <FiLogOut size={20} />
+          </Button>
+        </Flex>
+      )}
+      {!user && (
+        <Link
+          as={RouterLink}
+          to={"/auth"}
+          onClick={() => setAuthScreen("signup")}
+        >
+          Sign up
+        </Link>
+      )}
+
+      {/* Theme Toggle and Music Button */}
       <Flex alignItems={"center"} gap={4}>
         <Tooltip label="Toggle Theme" placement="top" hasArrow>
           <Image
@@ -51,32 +83,17 @@ const Header = () => {
             onClick={toggleColorMode}
           />
         </Tooltip>
-        <MusicButton /> {/* Add the MusicButton */}
-        {user && (
-          <>
-            <Link as={RouterLink} to={`/${user.username}`}>
-              <RxAvatar size={24} />
-            </Link>
-            <Link as={RouterLink} to={`/chat`}>
-              <BsFillChatQuoteFill size={20} />
-            </Link>
-            <Link as={RouterLink} to={`/settings`}>
-              <MdOutlineSettings size={20} />
-            </Link>
-            <Button size={"xs"} onClick={logout}>
-              <FiLogOut size={20} />
-            </Button>
-          </>
-        )}
-        {!user && (
-          <Link
-            as={RouterLink}
-            to={"/auth"}
-            onClick={() => setAuthScreen("signup")}
+        <Tooltip label="LISTEN TO MUSIC" placement="top" hasArrow>
+          <Button
+            size="sm"
+            onClick={handleMusicRedirect}
+            bg={"blue.500"}
+            color={"white"}
+            _hover={{ bg: "blue.700" }}
           >
-            Sign up
-          </Link>
-        )}
+            🎵
+          </Button>
+        </Tooltip>
       </Flex>
     </Flex>
   );
